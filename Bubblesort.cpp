eda27f0847ef9795ef0fc70e3339b3e97187ba45
TANMAY KUMAR MALLICK <tanmaykumar5147@gmail.com>
2026-03-02 20:08:01 +0530

    Add files via upload

---
+++ Bubblesort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {4, 5, 1, 2, 3};
+    int size = 5;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int c = 0; c < size; c++)
+    {
+        cout << arr[c] << " ";
+    }
+
+    return 0;
+}
